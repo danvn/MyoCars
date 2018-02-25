1a3120a5c0f15394fccfb3d04cda39d240496e8e
danvn <dan.nguyen7@yahoo.com>
2018-02-24 23:15:02 -0700

    first commit

---
+++ Google Drive/Code/CrossPlatformForms/CrossPlatformForms/NamePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CrossPlatformForms
+{
+	public class NamePage : ContentPage
+	{
+		public NamePage()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
---
+++ Google Drive/DevelopIntelligence/Module 3 - Cross-Platform Mobile Application Development with Xamarin/Mobile3_Lab1_Android/Mobile3_Lab1_Android/ListItem.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Mobile3_Lab1_Android
+{
+	[Activity(Label = "ListItem")]
+	public class ListItem : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+		}
+	}
+}
